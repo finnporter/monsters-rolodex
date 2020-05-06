23614bc777a6691fcd5f12b1ed0e7d1b4a032450
Finn Porter <iamfinnporter@gmail.com>
2020-05-06 11:45:37 +0100

    Updates

---
+++ precache-manifest.3d94f575919c07eddfd96e8ef3796e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da29ef7291f3b38f91ff8594373f85ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "86f1e6e7496d1449cae2",
+    "url": "/monsters-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f1e6e7496d1449cae2",
+    "url": "/monsters-rolodex/static/js/main.836083a8.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
